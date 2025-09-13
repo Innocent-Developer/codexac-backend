f696d47c7963fe07664fcb5fd362193b18b813da
abubakkar <abubakkarsajid4@gmail.com>
2025-09-13 15:19:06 +0500

    Work on serer

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./db/dbconnect.js";
-import router from "./routers/router.js";
+import {connectDB}  from "./db/dbconnect";
+import router from "./routers/router";
 import cron from "node-cron";
-import { applyDailyInterest } from "./stakeCoin/stakeCoins.js";
+import { applyDailyInterest } from "./stakeCoin/stakeCoins";
+const PORT = Number(process.env.PORT || 4000);
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT || 4000);
+
 
 const app = express();
 connectDB();
@@ -15,17 +16,16 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
-// ✅ Default route - show server running message on webpage
 app.get("/", (req, res) => {
   res.send(`<h1>🚀 Server running on http://localhost:${PORT} — env=${process.env.NODE_ENV}</h1>`);
 });
 
-// stack method every midnight
+//stack method every midnight
 cron.schedule("0 0 * * *", async () => {
-  console.log("Running daily interest job...");
+  console.log(" Running daily interest job...");
   await applyDailyInterest();
 });
-
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT} — env=${process.env.NODE_ENV}`);
-});
+  
+  console.log(`Server running on http://localhost:${PORT} — env=${process.env.NODE_ENV}`);
+})
