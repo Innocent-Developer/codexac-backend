ba857a8be7822fa16553eed904c5bf24a50897bd
abubakkar <abubakkarsajid4@gmail.com>
2025-09-13 15:24:39 +0500

    Work on users

---
+++ src/account/getUsers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { User } from "../models/user.model";
+
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await User.find({}, { 
+      _id: 0, 
+      address: 1, 
+      totalCoins: 1 
+    }).lean();
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
